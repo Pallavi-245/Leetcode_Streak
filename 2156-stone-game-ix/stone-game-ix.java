43b3f0c68f850727637931ec09aa16d98f4e50c9
Pallavi M <196095660+Pallavi-245@users.noreply.github.com>
2026-08-16 22:51:13 +0530

    Time: 4 ms (75.68%) | Memory: 118.7 MB (8.78%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnt=new int[3];
+        for(int X: stones){
+            cnt[X%3]++;
+        }
+        int a=cnt[1];
+        int b=cnt[2];
+        int c=cnt[0];
+
+        if(c%2==0){
+            return a>0 && b>0;
+        }
+        return Math.abs(a-b)>2;
+    }
+}
